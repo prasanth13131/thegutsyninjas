c3b2c8014eacc55a415f1339f048b25ad79de008
Administrator <Administrator@scsbwin-135870.ITTPA.EBIZ.VERIZON.COM>
2015-10-29 06:27:35 -0700

    Prasanth - :D

--- WebContent/googlemap.js
+++ WebContent/googlemap.js
@@ -1,3 +1,5 @@
+
+
 function initialize() {
 	if (navigator.geolocation) {
         navigator.geolocation.watchPosition(showPosition);
@@ -6,18 +8,41 @@ function initialize() {
      }
 }
 
-
-    
 function showPosition(position) {
     //alert(position.coords.latitude+' , '+position.coords.longitude);
     //'38.9847719','-77.5619419' - guindy
+	var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
     var mapProp = {
-		    center:new google.maps.LatLng(position.coords.latitude,position.coords.longitude),
+		    center:latlng,
 		    zoom:15,
 		    mapTypeId:google.maps.MapTypeId.ROADMAP
 		  };
     
     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    
+    new google.maps.InfoWindow({
+   	   content:'Bob Marley'
+   	   }).open(map,new google.maps.Marker({position: {lat: 13.003196, lng: 80.200601}, map: map}));
+		 new google.maps.InfoWindow({
+       	   content:'Arumugam'
+       	   }).open(map,new google.maps.Marker({position: {lat: 13.006102, lng: 80.203112}, map: map}));
+		 new google.maps.InfoWindow({
+       	   content:'Radhika'
+       	   }).open(map,new google.maps.Marker({position: {lat: 13.007440, lng: 80.201234}, map: map}));
+		 new google.maps.InfoWindow({
+       	   content:'Babu'
+       	   }).open(map,new google.maps.Marker({position: {lat: 13.013196, lng: 80.203601}, map: map}));
+		 
+		 var myCircle = new google.maps.Circle({
+		 	   center:latlng,
+		 	   radius:500,
+		 	   strokeColor:"#0000FF",
+		 	   strokeOpacity:0.8,
+		 	   strokeWeight:2,
+		 	   fillColor:"#000000",
+		 	   fillOpacity:0.4
+		 	 	});
+				 myCircle.setMap(map);	
 		 
 }
 
